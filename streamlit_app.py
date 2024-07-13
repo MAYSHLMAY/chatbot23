914ac9daffbad82a342d2bafa655b8a5dfb5188e
Mikiyas Dawit <90477617+MAYSHLMAY@users.noreply.github.com>
2024-07-13 15:01:50 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
-st.title("Welcome to My ChatBot App")
+st.title("Welcome to Blogga ChatBot")
 
 # Replicate Credentials
 with st.sidebar:
