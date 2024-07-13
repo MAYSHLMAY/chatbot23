eadc6a15c09cacd618f07c35c361d110380de2ff
Mikiyas Dawit <90477617+MAYSHLMAY@users.noreply.github.com>
2024-07-13 14:01:56 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,13 @@ import streamlit as st
 import replicate
 import os
 
-# App title
-st.set_page_config(page_title="🦙💬 Llama 2 Chatbot")
+st.set_page_config(
+    page_title="My ChatBot",
+    layout="centered",
+    initial_sidebar_state="expanded",
+)
+
+st.title("Welcome to My ChatBot App")
 
 # Replicate Credentials
 with st.sidebar:
