469b212d8b3a407f3ddb8e297c750a26cc35502d
Mikiyas Dawit <educationforlife424@gmail.com>
2024-07-14 14:36:41 +0300

    done

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import json
 import re  # Importing re for regular expressions
 
 st.set_page_config(
-    page_title="My ChatBot",
+    page_title="BlogBLAST Chatbot",
     layout="centered",
     initial_sidebar_state="expanded",
 )
